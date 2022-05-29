2290212d200d87229070129c27e6e27c84c339a9
Gaudhiwaa Hendrasto <88303669+gaudhiwaa@users.noreply.github.com>
2022-05-29 21:32:36 +0700

    Jawaban Nomor 5

--- F_5025201066.r
+++ F_5025201066.r
@@ -117,5 +117,50 @@ TukeyHSD(aov(model1))
 
 library(ggplot2)
 ggplot(dataoneway, aes(x = Group, y = Length)) + geom_boxplot(fill = "grey80", colour = "black") + 
-  scale_x_discrete() + xlab("Treatment Group") +  ylab("Length (cm)")
+scale_x_discrete() + xlab("Treatment Group") +  ylab("Length (cm)")
 
+# 5
+# 5. A
+
+install.packages("multcompView")
+library(readr)
+library(ggplot2)
+library(multcompView)
+library(dplyr)
+
+GTL <- read_csv("GTL.csv")
+head(GTL)
+
+str(GTL)
+
+qplot(x = Temp, y = Light, geom = "point", data = GTL) +
+  facet_grid(.~Glass, labeller = label_both)
+
+# 5. B.
+
+GTL$Glass <- as.factor(GTL$Glass)
+GTL$Temp_Factor <- as.factor(GTL$Temp)
+str(GTL)
+
+anova <- aov(Light ~ Glass*Temp_Factor, data = GTL)
+summary(anova)
+
+# 5. C.
+data_summary <- group_by(GTL, Glass, Temp) %>%
+  summarise(mean=mean(Light), sd=sd(Light)) %>%
+  arrange(desc(mean))
+print(data_summary)
+
+# 5. D.
+tukey <- TukeyHSD(anova)
+print(tukey)
+
+# 5. E.
+tukey.cld <- multcompLetters4(anova, tukey)
+print(tukey.cld)
+
+cld <- as.data.frame.list(tukey.cld$`Glass:Temp_Factor`)
+data_summary$Tukey <- cld$Letters
+print(data_summary)
+
+write.csv("GTL_summary.csv")
